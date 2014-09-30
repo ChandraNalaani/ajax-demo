8d6f40ce781910c8f7d1bc0335e30be285326a71
Chandra Nalaani <cnalaani@gmail.com>
2014-09-30 15:18:22 -0400

    Check readyState before sending

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var request = new XMLHttpRequest();
 
 request.open('GET', 'data.txt');
-request.send();
-if (request.status===200) {
-	console.log(request);
-	document.writeln(request.responseText);
+request.onreadystatechange = function() {
+	if ((request.readyState===4) && (request.status===200)) {
+		console.log(request);
+		document.writeln(request.responseText);
+	}
 }
+request.send();
