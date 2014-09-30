86f2cac683402b771eb9e891e87279807c573e25
Chandra Nalaani <cnalaani@gmail.com>
2014-09-30 15:13:10 -0400

    Reset to asynchronous default

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var request = new XMLHttpRequest();
 
-request.open('GET', 'data.txt', false);
+request.open('GET', 'data.txt');
 request.send();
 if (request.status===200) {
 	console.log(request);
